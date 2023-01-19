c4b9b4ee5379277bcc3d061ab03e0a32b235c737
Yun Jang <jang.yunj7@gmail.com>
2023-01-19 16:14:34 -0500

    initial commit

---
+++ ACalculator/ACalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Enter first number: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter an operator: ");
+char op = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double num2 = double.Parse(Console.ReadLine());
+
+
+if (op == '+')
+{
+    Console.WriteLine(num1 + num2);
+}
+else if (op == '-')
+{
+    Console.WriteLine(num1 - num2);
+}
+else if (op == '*')
+{
+    Console.WriteLine(num1 * num2);
+}
+else if (op == '/')
+{
+    Console.WriteLine(num1 / num2);
+}
+else
+{
+    Console.WriteLine("invalid operator");
+}
